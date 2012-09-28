cb48eeff6baf79ab7bd39d4f6657bf93ff6090d1
Thomas Goyne <tgoyne@gmail.com>
2012-09-28 11:58:45 -0700

    Convert site to middleman + haml

---
+++ config.rb
@@ -0,0 +1,11 @@
+activate :directory_indexes
+activate :automatic_image_sizes
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  activate :minify_javascript
+  activate :cache_buster
+end
