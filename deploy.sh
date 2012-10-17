e6e9bae7582de89137a2326a1777158d2c16a79b
Thomas Goyne <tgoyne@gmail.com>
2012-10-17 16:46:00 -0700

    Add update checker files

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ middleman build
 rsync -ae ssh build/docs/ www-write@aegisub.org:/var/www/docs.aegisub.org/
 rsync -ae ssh build/www/ www-write@aegisub.org:/var/www/www.aegisub.org/
 rsync -ae ssh build/static/ www-write@aegisub.org:/var/www/static.aegisub.org/
+rsync -ae ssh build/updates/ www-write@aegisub.org:/var/www/updates.aegisub.org/
