1e2d70a93a751f7b081c8e1d4e13352e2385b7b2
Thomas Goyne <tgoyne@gmail.com>
2012-10-22 20:53:37 -0700

    Fix wiki link lookups for pages with slashes

--- helpers/wiki_link.rb
+++ helpers/wiki_link.rb
@@ -1,19 +1,36 @@
 module WikiLink
   def page_url page
-    suffix = "/#{page}/"
+    path_parts = page.split '/'
     resource = current_resource
-    while resource
-      resource.children.each do |r|
-        return r.url if r.url.end_with? suffix
-      end
-      resource = resource.parent
-    end
-    print "Page not found: #{page}\n"
-    nil
+
+    url   = check_node(resource, path_parts)
+    url ||= check_node(resource.parent, path_parts)
+    url ||= check_node(root_resource(resource), path_parts)
+
+    print "Page not found: #{page}\n" unless url
+    url
   end
 
   def wiki_link page, text
     text = page.gsub('_', ' ') if text.blank?
     link_to(text, page_url(page))
+  end
+
+  private
+  def check_node node, parts
+    return node.url + parts[0] if parts[0].start_with? '#'
+
+    node.children.each do |c|
+      if c.url.split('/').last == parts[0]
+        return c.url if parts.length == 1
+        return check_node c, parts.drop(1)
+      end
+    end
+    nil
+  end
+
+  def root_resource node
+    node = node.parent while node.parent
+    node
   end
 end
