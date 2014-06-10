31000b666d07d71a7ec44eaa324954e3e803fd84
Thomas Goyne <tgoyne@gmail.com>
2014-06-10 07:21:25 -0700

    Fix wiki link generation with multiple manual versions

--- helpers/wiki_link.rb
+++ helpers/wiki_link.rb
@@ -2,10 +2,11 @@ module WikiLink
   def page_url page
     path_parts = page.split '/'
     resource = current_resource
+    prefix = current_resource.url.match(/\/docs\/[^\/]+\//)[0]
 
-    url   = check_node(resource.children, path_parts)
-    url ||= check_node(resource.siblings, path_parts)
-    url ||= check_node(sitemap.resources, path_parts)
+    url   = check_node(resource.children, path_parts, prefix)
+    url ||= check_node(resource.siblings, path_parts, prefix)
+    url ||= check_node(sitemap.resources, path_parts, prefix)
 
     print "Page not found: #{page}\n" unless url
     url
@@ -26,12 +27,12 @@ module WikiLink
   end
 
   private
-  def check_node nodes, parts
+  def check_node nodes, parts, prefix
     nodes.each do |c|
-      if c.url.split('/').last.casecmp(parts[0]) == 0
+      if c.url.start_with? prefix and c.url.split('/').last.casecmp(parts[0]) == 0
         return c.url if parts.length == 1
         return c.url + parts[1] if parts[1].start_with? '#'
-        return check_node c.children, parts.drop(1)
+        return check_node c.children, parts.drop(1), prefix
       end
     end
     nil
